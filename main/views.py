c4c54412af30fc073769b905014a38311f688e62
Ariarti <ariarti@list.ru>
2020-11-14 17:53:59 +0300

    add main app

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h3>Проверка работы</h3")
+
+def about(request):
+    return HttpResponse("<h3>Страница про нас</h3")
