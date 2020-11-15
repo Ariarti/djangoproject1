bd45777a8a7b4e3c258b133ad034b5caeb4d64f5
Ariarti <ariarti@list.ru>
2020-11-15 17:02:56 +0300

    add links

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("<h3>Проверка работы</h3")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h3>Страница про нас</h3")
+    return render(request, 'main/about.html')
